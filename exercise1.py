d21655b30d343803bee55e470176e36195fc4709
EY-X <e.yosefx128@gmail.com>
2019-07-30 01:11:43 -0400

    First Commit

---
+++ exercise1.py
@@ -0,0 +1,20 @@
+doc = 'How to be rich'
+drama = 'notebook'
+comedy = 'friday'
+
+print('Do you like documentaries?')
+doc_input = input().lower
+
+print('Do you like drama?')
+drama = input().lower
+
+print('Do you like comedy?')
+comedy = input().lower
+
+if doc_input == yes:
+    print(f'I reccomend {doc}')
+elif drama == yes:
+    print(f'I reccomend {drama}')
+elif comedy == yes:
+    print(f'I reccomend {comedy}')
+    
